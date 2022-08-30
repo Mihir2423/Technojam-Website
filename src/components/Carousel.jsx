42308e26b2d3c032aa71569e1e3fc5503ba1d106
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-08-31 00:42:57 +0530

    carousel work

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,51 +1,70 @@
 import { Box, Heading, Image, Text } from '@chakra-ui/react';
-import { Pagination } from "swiper";
+import { Autoplay, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+
+import bootcampsImage from '../assets/Bootcamps/DSC_0109.JPG';
+import dexterixImage from '../assets/Dexterix3/IMG_0039.JPG';
+import flutterEventImage from '../assets/Flutter Days/IMG_7810.jpg';
+import promotionalEventImage from '../assets/Promotional Events/20210225145311_IMG_6914.jpg';
 
 const data = [
     {
-        imageUrl: 'https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?w=2000',
-        title: 'This is a event slider 1',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A dolorem qui at labore vitae laboriosam accusamus. Asperiores consectetur laboriosam praesentium officiis, qui vero itaque fugiat, perferendis, maiores ipsa incidunt est expedita beatae facilis.'
+        imageUrl: promotionalEventImage,
+        title: 'Promotional Events',
     },
     {
-        imageUrl: 'https://www.elegantthemes.com/blog/wp-content/uploads/2018/04/Best-Code-and-Text-Editors.png',
-        title: 'This is a event slider 2',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A dolorem qui at labore vitae laboriosam accusamus. Asperiores consectetur laboriosam praesentium officiis, qui vero itaque fugiat, perferendis, maiores ipsa incidunt est expedita beatae facilis.'
-    }
+        imageUrl: flutterEventImage,
+        title: 'Flutter Days',
+    },
+    {
+        imageUrl: dexterixImage,
+        title: 'Dexterix 3',
+    },
+    {
+        imageUrl: bootcampsImage,
+        title: 'Bootcamps',
+    },
 ];
 
 
 const Carousel = () => {
     return (
-        <Swiper
-            pagination={{
-                dynamicBullets: true,
-            }}
-            modules={[Pagination]}
-            className="mySwiper"
-            loop={true}
-        >
-            {data.map((item, index) => (
+        <Box>
+            <Heading textAlign={'center'} py={10} as='h2'>
+                EVENTS
+            </Heading>
 
-                <SwiperSlide>
-                    <Box display={'flex'} alignItems={'center'} justifyContent={'center'} flexDir={'column'} h={'100%'} pb={12} mx={'20%'}>
-                        <Image
-                            boxSize={500}
-                            src={item.imageUrl}
-                            alt=""
-                        />
-                        <Heading>
-                            {item.title}
-                        </Heading>
-                        <Text>
-                            {item.text}
-                        </Text>
-                    </Box>
-                </SwiperSlide>
-            ))}
-        </Swiper>
+            <Swiper
+                pagination={{
+                    dynamicBullets: true,
+                    clickable: true
+                }}
+                modules={[Autoplay, Pagination]}
+                className="mySwiper"
+                loop={true}
+                autoplay={{
+                    delay: 3500,
+                    disableOnInteraction: false,
+                }}
+            >
+                {data.map((item, index) => (
+                    <SwiperSlide key={index}>
+                        <Box display={'flex'} alignItems={'center'} justifyContent={'center'} flexDir={'column'} h={'100%'} pb={12} mx={'20%'}>
+                            <Image
+                                width={800}
+                                height={450}
+                                src={item.imageUrl}
+                                alt={item.title}
+                            />
+                            <Heading as='h6' size='md' pt='5'>
+                                {item.title}
+                            </Heading>
+                        </Box>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
 
+        </Box>
     );
 };
 
